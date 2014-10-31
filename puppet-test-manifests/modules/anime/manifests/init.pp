d91afb1bf9a79d3e2c20780a9426852a9ce7d3a8
Tim Payne <tpayne@serena.com>
2014-10-31 15:25:14 +0000

    Modified examples

--- puppet-test-manifests/modules/anime/manifests/init.pp
+++ puppet-test-manifests/modules/anime/manifests/init.pp
@@ -57,13 +57,101 @@ class anime::refresh {
     }
 }
 
+class anime::runCmd {
+    notify {"This is a command run based of a resource update...":}
+    exec { 'doSomeCommand':
+      path        => '/usr/local/bin:/usr/bin:/bin',
+      command     => 'test 1=1',
+      creates    => '/tmp/finished',
+      #onlyif     => 'checkstatus.sh',
+      #unless     => 'checkstatus.sh',
+      refreshonly => true,
+      subscribe   => File['animeJpgTest'],
+    }
+
+    file { '/tmp/testFile.txt':
+        owner   => 'root',
+        mode    => '0644',
+        content => 'hello world',
+    }
+
+}
+
+class anime::testCondition {
+    # This is intended to show some test conditions...
+    if ($osfamily == 'Darwin') {
+        warning('This is a Mac and so does not work')
+    } else {
+        warning('This is NOT a Mac so will work')
+    }
+
+    case $operatingsystem {
+        'Darwin': { require anime::thisIsAMac }
+        default:  { require anime::thisIsSomethingElse }
+    }
+
+    unless $operatingsystem != 'Darwin' {
+        notice('This is an UNLESS test')
+    }
+
+    $contents = $::operatingsystem ? {
+        'centos' => 'Best OS in the world',
+        'debian' => 'fail',
+        'Darwin' => 'Godlike',
+        default  => 'A default OS',
+    }
+
+    file { '/tmp/osTest':
+        ensure  => file,
+        content => $contents
+    }
+}
+
+class anime::scoping::vars {
+    $myVar = "Test"
+}
+
+class anime::scoping {
+    include anime::scoping::vars
+    notice("This is a variable ${anime::scoping::vars::myVar}")
+}
+
+class anime::thisIsAMac {
+    warning("This is a Mac - really...")
+}
+
+class anime::thisIsSomethingElse {
+    warning("This is something else...")
+}
+
+class anime::tagTest {
+    file { '/tmp/thisFileTag':
+        ensure  => file,
+        content => 'Some data',
+        tag => 'tagTest' 
+    }
+}
+
+class anime::scheduleTest {
+    # Scheduling test...
+    schedule { 'outofhours':
+      period => 'daily',
+      range  => '0100-0300',
+    }
+
+    service { 'foo':
+      ensure   => running,
+      schedule => 'outofhours',
+    }
+}
+
 class anime::base {
-    notify { "This is an anime module...": }
+	notify { "This is an anime module...": }
     $isThisAMac = $osfamily ? {
         'Darwin' => 'Yes',
         default  => 'No',
     }
-    
+	
     notify { "This is a Mac: ${isThisAMac}":}
     createUser{"newUser":}
 }
@@ -85,4 +173,8 @@ class anime {
 
     require anime::base::test
     require anime::refresh
+    require anime::runCmd
+    require anime::testCondition
+    require anime::scoping
+    require anime::tagTest
 }
