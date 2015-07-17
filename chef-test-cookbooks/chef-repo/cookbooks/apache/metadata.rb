cb53b8518ec6746a8d8abd473219438a99cf111b
Tim Payne <tpayne@serena.com>
2015-07-17 16:29:02 +0100

    Missing files

--- chef-test-cookbooks/chef-repo/cookbooks/apache/metadata.rb
+++ chef-test-cookbooks/chef-repo/cookbooks/apache/metadata.rb
@@ -5,3 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+#Add dependencies to cookbooks...
+
+#depends		'my-company'
