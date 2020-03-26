cb3613567adf6c90643340684fefca17d22bd17e
Tim Payne <tim.tpayne@gmail.com>
2020-03-26 22:54:25 +0000

    Add run examples script to show use

--- ansible-test-playbooks/run_sample.sh
+++ ansible-test-playbooks/run_sample.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
+#!/bin/sh -xv
 
+echo "Run some samples..."
 ansible-playbook -i ansible_hosts simple_playbook.yml
+ansible-playbook -i ansible_hosts playbook_batch.yml
+ansible-playbook -i ansible_hosts playbook_delegate.yml
+ansible-playbook -i ansible_hosts playbook_env.yml
+ansible-playbook -i ansible_hosts playbook_errorhandling.yml
+
+ansible-playbook -i ansible_hosts playbook_factsassert.yml
+ansible-playbook -i ansible_hosts playbook_lists.yml 
+ansible-playbook -i ansible_hosts playbook_module.yml
+ansible-playbook -i ansible_hosts playbook_rc.yml
+
+ansible-playbook -i ansible_hosts playbook_nestedloops.yml
+
+echo "You need to press return after the polling..."
+ansible-playbook -i ansible_hosts playbook_polling.yml 
+
